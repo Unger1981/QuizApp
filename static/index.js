16afd807551f36b66595cbfaefe4f03b6ea9f389
David Unger <David Unger@outlook.com>
2024-06-02 21:21:32 +0200

    Frontend Changes and logic for Selection

--- static/index.js
+++ static/index.js
@@ -1,4 +1,34 @@
-//Event Functions
+
+
+
+// // Functional Variables
+// let contentArray =[1,2,3,4,5,6]
+let selectedCategory ="";
+let selectedDifficult ="";
+let allOptionSelected = false;
+
+// //DOM Registration
+
+// 
+// let contentArea = document.querySelector(".contentArea")
+let navBar = document.querySelector(".navBar");
+let selectionBox = document.querySelector(".selectionBox")
+let infoParaContainer = document.querySelector(".infoParaContainer")
+let infoPara = document.querySelector(".infoPara")
+// let allCategoryElements=document.querySelectorAll(".categoryPara")
+let applyButton = document.querySelector(".applyButtonDisabled")
+let difficultButtons = document.querySelectorAll(".difficultPara")
+
+
+//Added Event handler
+
+navBar.addEventListener("touchstart", handleTouchNav);
+applyButton.addEventListener("touchstart",handleApplyButton )
+difficultButtons.forEach(element => {
+  element.addEventListener("touchstart", handleDifficultButton)
+  
+});
+// //Event Functions
 
 function handleTouchNav(event){
   console.log("touch")
@@ -9,6 +39,7 @@ function handleTouchNav(event){
 
 function handleTouchCategory(event){
   selectedCategory = event.target.innerText;
+  checkSelection();
   console.log(selectedCategory);
   //allCategoryElements erst in der Funktion initialisiert, da via Async Await fetched und generiert
   let allCategoryElements=document.querySelectorAll(".categoryPara")
@@ -18,47 +49,57 @@ function handleTouchCategory(event){
     // Überprüfen, ob das Element die Klasse 'categoryParaSelected' hat
     if (element.classList.contains("categoryParaSelected")&& element !== event.target) {
       
-      element.classList.toggle("categoryParaSelected");
+      element.classList.remove("categoryParaSelected");
     }
   });
 
-  event.target.classList.toggle("categoryParaSelected")
+  event.target.classList.add("categoryParaSelected")
   
   
 };
 
-// Functional Variables
-let contentArray =[1,2,3,4,5,6]
-let selectedCategory =""
-
-//DOM Registration
-
-let navBar = document.querySelector(".navBar");
-let contentArea = document.querySelector(".contentArea")
-let selectionBox =document.querySelector(".selectionBox")
-let allCategoryElements=document.querySelectorAll(".categoryPara")
-let applyButton = document.querySelector(".applyButton")
-
-
-navBar.addEventListener("touchstart", handleTouchNav);
-applyButton.addEventListener("touchstart", )
-
-
-
-
-for (let i=0; i<contentArray.length; i++){
-let contentBox = document.createElement("div")
-contentBox.classList.add("contentBox")
-contentArea.appendChild(contentBox)
-console.log(i)
+// Function for Apply Button Event 
+function handleApplyButton(event){
+if(allOptionSelected== true) {
+  console.log("true")
+}
 
 }
+
+function handleDifficultButton(event) {
+  selectedDifficult = event.target.innerText;
+  checkSelection();
+  console.log(selectedDifficult);
+
+  difficultButtons.forEach(element => {
+    if (element.classList.contains("categoryParaSelected")) {
+      element.classList.remove("categoryParaSelected");
+    }
+  });
+
+  event.target.classList.add("categoryParaSelected");
+}
+  
+
+
+
+
+
+
+
+// for (let i=0; i<contentArray.length; i++){
+// let contentBox = document.createElement("div")
+// contentBox.classList.add("contentBox")
+// contentArea.appendChild(contentBox)
+// console.log(i)
+
+// }
 
 // URL API TRIVIA CATEGORIES
 let url = "http://127.0.0.1:5000/categories";
 
 
-// Async Function fetching Categories
+// Async Function fetching Categories with embedded append in DOM and event listener handleTouchCategory
 async function autoFetch(url){
 
 const response = await fetch(url);
@@ -88,3 +129,14 @@ console.log(element)
 
 appendOptions(url)
 
+//Function 
+
+
+function checkSelection(){
+if(selectedCategory!=="" && selectedDifficult!==""){
+  allOptionSelected = true;
+  applyButton.classList.add("applyButtonEnabled")
+  console.log("all selected")
+}
+
+}
